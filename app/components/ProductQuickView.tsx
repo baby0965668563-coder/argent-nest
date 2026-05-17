57bb34299122463e85fbf5e1c32996f1e1dbb808
baby0965668563-coder <baby0965668563@gmail.com>
2026-05-17 11:42:11 +0800

    Update ProductQuickView.tsx

--- app/components/ProductQuickView.tsx
+++ app/components/ProductQuickView.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import ProductQuickView from "./components/ProductQuickView";
 
 function parseOptions(optionsText: string) {
   if (!optionsText) return [];
