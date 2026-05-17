a3508dbe5761b3b3a0137e80a6598f110b04748d
baby0965668563-coder <baby0965668563@gmail.com>
2026-05-17 21:48:12 +0800

    Update page.tsx

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -297,6 +297,7 @@ const lineUrl = `https://line.me/R/oaMessage/@929santn/?${encodeURIComponent(
 <AddToCartButton
   product={product}
   selectedOptions={selectedOptions}
+customerNote={customerNote}
   disabled={optionGroups.length > 0 && !isOptionsComplete}
 />
 
